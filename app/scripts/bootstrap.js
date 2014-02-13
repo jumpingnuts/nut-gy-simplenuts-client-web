1b3a81ee6c4ffa27ba7f40bbefe06748853ed096
deka58 <sangho.kwon@jumpingnuts.com>
2014-02-13 03:04:45 +0000

    bower json update

--- app/scripts/bootstrap.js
+++ app/scripts/bootstrap.js
@@ -9,7 +9,6 @@ require.config({
     angularCookies: '../bower_components/angular-cookies/angular-cookies',
     angularMD5: '../bower_components/angular-md5/angular-md5',
     angularBootstrap: '../bower_components/angular-bootstrap/ui-bootstrap-tpls.min',
-    text: '../bower_components/requirejs-text/text',
     jquery: '../bower_components/jquery/jquery',
     kakao: 'lib/kakao.link.min',
   },
